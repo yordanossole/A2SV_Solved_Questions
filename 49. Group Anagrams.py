b8f82cc2d2f71fe7dffb09dc1743d83479a11a76
yordanossole <yordanos.sole@gmail.com>
2026-02-04 16:50:13 +0300

    49. Group Anagrams.py

---
+++ 49. Group Anagrams.py
@@ -0,0 +1,9 @@
+from collections import Counter, defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        group = defaultdict(list)
+        for word in strs:
+            key = "".join(sorted(word))
+            group[key].append(word)
+
+        return list(group.values())
