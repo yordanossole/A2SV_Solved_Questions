ef79fb84a7e911a7ae4389636882d3ddd442b00f
yordanossole <yordanos.sole@gmail.com>
2026-02-20 10:08:50 +0300

    885_Spiral_Matrix_III.py

---
+++ 885_Spiral_Matrix_III.py
@@ -0,0 +1,21 @@
+# leetcode
+class Solution:
+    def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
+        directions = [[0,1], [1,0], [0,-1], [-1,0]]
+        r, c = rStart, cStart
+        steps = 1
+        i = 0
+        result = []
+
+        while len(result) < rows * cols:
+            for _ in range(2):
+                dr, dc = directions[i]
+                for _ in range(steps):
+                    if 0 <= r < rows and 0 <= c < cols:
+                        result.append([r,c])
+                    r += dr
+                    c += dc
+                i = (i+1) % 4
+            steps += 1
+
+        return result
