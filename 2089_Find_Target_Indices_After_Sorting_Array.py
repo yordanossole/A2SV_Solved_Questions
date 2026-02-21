0827b5c84659a175e87ae6d86e543c93a0a9bf64
yordanossole <yordanos.sole@gmail.com>
2026-02-21 16:22:32 +0300

    2089_Find_Target_Indices_After_Sorting_Array.py

---
+++ 2089_Find_Target_Indices_After_Sorting_Array.py
@@ -0,0 +1,5 @@
+# leetcode
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        nums.sort()
+        return [i for i, num in enumerate(nums) if num == target]
