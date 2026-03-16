ab8bb331ecf486220161434adc5ea67772e40870
yordanossole <yordanos.sole@gmail.com>
2026-03-16 18:09:20 +0300

    improved by using caching

--- 509_Fibonacci_Number.py
+++ 509_Fibonacci_Number.py
@@ -1,9 +1,13 @@
 # leetcode
 class Solution:
-    def fib(self, n: int) -> int:
+    def fib(self, n: int, memo: dict = {}) -> int:
+        # improved by using caching
+        if n in memo:
+            return memo[n]
         if n == 1:
             return 1
         if n == 0:
             return 0
         
-        return self.fib(n-1) + self.fib(n-2)
+        memo[n] = self.fib(n-1, memo) + self.fib(n-2, memo)
+        return memo[n]
