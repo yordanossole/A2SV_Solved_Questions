38226ea3e3f91661f64dbc70646c0fda810e003d
yordanossole <yordanos.sole@gmail.com>
2026-03-16 21:26:08 +0300

    2366_Minimum_Replacements_to_Sort_the_Array.py

---
+++ 2366_Minimum_Replacements_to_Sort_the_Array.py
@@ -0,0 +1,18 @@
+# leetcode
+class Solution:
+    def minimumReplacement(self, nums: List[int]) -> int:
+        n = len(nums)
+        last = nums[-1]
+        ans = 0
+
+        for i in range(n-2, -1, -1):
+            if nums[i] > last:
+                t = nums[i] // last
+                if nums[i] % last:
+                    t += 1
+                last = nums[i] // t
+                ans += t - 1
+            else:
+                last = nums[i]
+        
+        return ans
