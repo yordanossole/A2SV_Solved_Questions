3b052c0fdde92017b3e2ea4cb8eebbb69c6e9b85
yordanossole <yordanos.sole@gmail.com>
2026-03-16 17:30:44 +0300

    342_Power_of_Four.py

---
+++ 342_Power_of_Four.py
@@ -0,0 +1,11 @@
+# leetcode
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        if n == 1:
+            return True
+        if n%4:
+            return False
+        
+        return self.isPowerOfFour(n/4)
