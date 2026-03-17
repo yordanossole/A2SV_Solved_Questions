b67f195b914a352e7f08360bfc07d83c829bee21
yordanossole <yordanos.sole@gmail.com>
2026-03-17 17:52:27 +0300

    50_Pow(x,_n).py

---
+++ 50_Pow(x,_n).py
@@ -0,0 +1,13 @@
+# leetcode
+class Solution:        
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+
+        if n < 0:
+            return 1 / self.myPow(x, -n) 
+
+        if n % 2:
+            return x * self.myPow(x*x, n//2)
+        else:
+            return self.myPow(x*x, n//2)
