4ed02d7c62919c123f4a1166d30dc132092cf282
Yordanos Solomon <117822242+yordanossole@users.noreply.github.com>
2026-03-27 19:40:28 +0300

    Add solution for Decode String

---
+++ leetcode/0394-decode-string/0394-decode-string.py
@@ -0,0 +1,23 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+
+        for c in s:
+            if c != ']':
+                stack.append(c)
+            else:
+                substr = []
+                while stack[-1] != '[':
+                    substr.append(stack.pop())
+
+                stack.pop()
+
+                num = []
+                while stack and stack[-1].isdigit():
+                    num.append(stack.pop())
+
+                num = int("".join(num[::-1]))
+                substr = "".join(substr[::-1])
+                stack.append(substr*num)
+        
+        return "".join(stack)
